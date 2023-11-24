287503353733ffbdf9d92cbc679cc7fe30d72062
Vijay-Hozo <vijay2304a@gmail.com>
2023-11-24 20:10:20 +0530

    test charts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import "./components/Login";
+import Graph from "./components/Graph";
+
 import SideBar from "./components/SideBar";
+
 
 function App() {
   return (
     <div className="h-screen w-screen">
       <SideBar />
+      <Graph/>
     </div>
   );
 }
