c2df37c998262e5e08227f643046ae1b0a12a326
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2023-11-24 17:18:32 +0530

    :tada: Client Setup done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="font-bold text-blue-400">My Assets</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    boxShadow: {
+      primary: "1px 1px 30px 0px rgba(0,0,0,0.15)",
+      "3xl": "1px 35px 60px -15px rgba(0, 0, 0, 0.3)",
+    },
+    fontWeight: {
+      bold: "600",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        geologica: ["Geologica", "sans-serif"],
+        gruppo: ["Gruppo", "sans-serif"],
+      },
+      colors: {
+        "black-90": "rgba(0,0,0,0.9)",
+        green: "#02B15A",
+        purple: "#6359E9",
+        red: "#EB001B",
+      },
+    },
+  },
+  plugins: [],
+};
